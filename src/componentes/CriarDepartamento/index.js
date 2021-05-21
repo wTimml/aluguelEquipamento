7c5e7c14c8094de35d650e02b6bf2c0cfdb36d31
Vinicius Jacik <vinicius-jacik@hotmail.com>
2021-05-20 22:07:18 -0300

    some changes

---
+++ src/componentes/CriarDepartamento/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CriarDepartamento = () => {
+
+    return(
+        <div>
+            <h1>Hello bitches</h1>
+        </div>
+    )
+}
+
+export default CriarDepartamento;
