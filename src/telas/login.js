45851bcfaaa4ec3bade0d499d4c44553656c39d0
Vinicius Jacik <vinicius-jacik@hotmail.com>
2021-05-26 22:04:02 -0300

    adicoes ao criar cliente e correcoes gerais no css

--- src/telas/login.js
+++ src/telas/login.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import CriarCliente from '../componentes/CriarCliente/CriarCliente'
-import De from '../componentes/CriarDepartamento/CriarDepartamento'
 
 const Login = () => {
 
     return(
         <div>
-            <De />
-            <CriarCliente />
         </div>
     )
 }
