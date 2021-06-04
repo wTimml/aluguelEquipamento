8fa71ef6d87bbad458977360496dcfc2d146a3c0
Vinicius Jacik <vinicius-jacik@hotmail.com>
2021-06-04 07:06:26 -0300

    finalização do criarItem, falta apenas integração

--- src/telas/tela de testes.js
+++ src/telas/tela de testes.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Botao1 from '../componentes/botoes/botao1'
+import CriarItem from '../componentes/CriarItem/CriarItem'
+import CriarCliente from '../componentes/CriarCliente/CriarCliente'
 
 export const TelaTestes = ()=>{
     return(
         <div>
-            
-            <Botao1>Disponivel</Botao1>
+            <CriarCliente />
+            <CriarItem />
         </div>
     )
 }
