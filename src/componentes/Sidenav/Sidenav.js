a4b5da0aa6e56aaf2635c7b3dc5c424493818182
wTimml <wtl25@hotmail.com>
2021-06-09 21:01:45 -0300

    sidenav adjust

--- src/componentes/Sidenav/Sidenav.js
+++ src/componentes/Sidenav/Sidenav.js
@@ -5,11 +5,11 @@ const Sidenav = () => {
     return(
         <div className="sidenav">
             <div className="sidenav-home">
-                <a href="/"><Home style={{fontSize:40}}/></a>
+                <a href="/"><Home className='sidenav-icons' style={{fontSize:20}}/> Home</a>
             </div>
 
             <div className="sidenav-dropdown1">
-                <a href="#"><AddBox style={{fontSize:20}}/> Cadastrar</a>
+                <a href="#"><AddBox className='sidenav-icons' style={{fontSize:20}}/> Cadastrar</a>
                 <div id="sidenav-dropdown1" className="sidenav-dropdown-content1">
                     <a href="/">Cliente</a>
                     <a href="/">Departamento</a>
@@ -19,11 +19,11 @@ const Sidenav = () => {
             </div>
 
             <div className="sidenav-dropdown2">
-                <a href="/" className="bar-item button"><Book style={{fontSize:20}}/> Reservar</a>
+                <a href="/" className="bar-item button"><Book className='sidenav-icons' style={{fontSize:20}}/> Reservar</a>
             </div>
 
             <div className="sidenav-dropdown3">
-                <a href="/" className="bar-item button"><LibraryBooks style={{fontSize:20}}/> Equipamentos</a>
+                <a href="/" className="bar-item button"><LibraryBooks  className='sidenav-icons' style={{fontSize:20}}/> Equipamentos</a>
             </div>
         </div>
     )
