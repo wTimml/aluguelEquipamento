d5b3bc4621c1af955d92243a464af09606caafbf
wTimml <wtl25@hotmail.com>
2021-06-15 23:00:29 -0300

    ajustes api

--- src/componentes/CriarDepartamento/CriarDepartamento.js
+++ src/componentes/CriarDepartamento/CriarDepartamento.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import api from '../../Service/api';
 import './styles.css'
 
 const CriarDepartamento = () => {
@@ -20,20 +20,22 @@ const CriarDepartamento = () => {
 
     const handleSubmit = async e => {
         e.preventDefault();
-        await axios.post('http://localhost:8080/departamentos', departamento)
+        api
+            .post('/departamentos', departamento)
             .then(response => setDepartamento(departamento.id = response.data.id));
-
     }
 
     return (<>
         <h1>Cadastrar Departamento</h1>
         <form onSubmit={handleSubmit}>
             <div className='inputBackground'>
-                <input
-                    label='Nome'
-                    placeholder='Nome'
-                    onChange={handleChange('nome')}
-                />
+                <div>
+                    <input
+                        label='Nome'
+                        placeholder='Nome'
+                        onChange={handleChange('nome')}
+                        />
+                </div>
             </div>
             <div className='row'>
                 <div className='error'>{aviso.aviso}</div>
