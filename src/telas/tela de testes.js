4d7c829d25d23c450308d71d32da488f96d59cd6
wTimml <wtl25@hotmail.com>
2021-06-15 21:47:37 -0300

    merged/conflicts fix

--- src/telas/tela de testes.js
+++ src/telas/tela de testes.js
@@ -1,22 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import Botao1 from '../componentes/botoes/botao1'
-import Equipamentos from '../componentes/Equipamentos/Equipamentos'
-=======
 import CriarItem from '../componentes/CriarItem/CriarItem'
 import CriarCliente from '../componentes/CriarCliente/CriarCliente'
->>>>>>> main
 
 export const TelaTestes = ()=>{
     return(
         <div>
-<<<<<<< HEAD
-            
-            <Equipamentos/>
-=======
             <CriarCliente />
             <CriarItem />
->>>>>>> main
         </div>
     )
 }
